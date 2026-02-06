c27dff698667d700a6ac6ebc2b83f375e3fd8f07
Martin Schilling <martin.schilling@med.uni-goettingen.de>
2026-02-06 16:16:58 +0100

    Annotation utils for common functions

--- flamingo_tools/intensity_annotation/annotation_utils.py
+++ flamingo_tools/intensity_annotation/annotation_utils.py
@@ -6,11 +6,106 @@ import imageio.v3 as imageio
 import napari
 import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QComboBox
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from magicgui import magicgui
 
-import flamingo_tools.intensity_annotation.annotation_utils as annotation_utils
+from elf.parallel.distance_transform import distance_transform
+from elf.parallel.seeded_watershed import seeded_watershed
+
 from flamingo_tools.measurements import compute_object_measures_impl, get_object_measures_from_table
 from flamingo_tools.s3_utils import get_s3_path
+
+
+class HistogramWidget(QWidget):
+    """Qt widget that draws/updates a histogram for one napari layer."""
+    def __init__(self, statistics, default_stat, bins: int = 32, parent=None):
+        super().__init__(parent)
+        self.bins = bins
+
+        # --- layout ------------------------------------------------------
+        self.fig, self.ax = plt.subplots(figsize=(4, 3), tight_layout=True)
+        self.canvas = FigureCanvasQTAgg(self.fig)
+
+        # We exclude the label id and the volume / surface measurements.
+        self.stat_names = statistics.columns[1:-2] if len(statistics.columns) > 2 else statistics.columns[1:]
+        self.param_choices = self.stat_names
+
+        self.param_box = QComboBox()
+        self.param_box.addItems(self.param_choices)
+        self.param_box.setCurrentText(default_stat)
+
+        self.refresh_btn = QPushButton("Refresh")
+        self.refresh_btn.clicked.connect(self.update_hist)
+
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Choose statistic:"))
+        layout.addWidget(self.param_box)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.refresh_btn)
+        self.setLayout(layout)
+
+        self.statistics = statistics
+        self.update_hist()  # initial draw
+
+    def update_hist(self):
+        """Redraw the histogram."""
+        self.ax.clear()
+
+        stat_name = self.param_box.currentText()
+
+        data = self.statistics[stat_name]
+        # Seaborn version (nicer aesthetics)
+        sns.histplot(data, bins=self.bins, ax=self.ax, kde=False)
+
+        self.ax.set_xlabel(f"{stat_name} Marker Intensity")
+        self.ax.set_ylabel("Count")
+        self.canvas.draw_idle()
+
+
+def _create_stat_widget(statistics, default_stat):
+    widget = HistogramWidget(statistics, default_stat)
+    return widget
+
+
+# Just dilate by 3 pixels.
+def _extend_seg_simple(seg, dilation):
+    block_shape = (128,) * 3
+    halo = (dilation + 2,) * 3
+
+    distances = distance_transform(seg == 0, block_shape=block_shape, halo=halo, n_threads=8)
+    mask = distances < dilation
+
+    seg_extended = np.zeros_like(seg)
+    seg_extended = seeded_watershed(
+        distances, seg, seg_extended, block_shape=block_shape, halo=halo, n_threads=8, mask=mask
+    )
+
+    return seg_extended
+
+
+def _create_mask(seg_extended, stain):
+    from skimage.transform import downscale_local_mean, resize
+
+    stain_averaged = downscale_local_mean(stain, factors=(16, 16, 16))
+    # The 35th percentile seems to be a decent approximation for the background subtraction.
+    threshold = np.percentile(stain_averaged, 35)
+    mask = stain_averaged > threshold
+    mask = resize(mask, seg_extended.shape, order=0, anti_aliasing=False, preserve_range=True).astype(bool)
+    mask[seg_extended != 0] = 0
+
+    # v = napari.Viewer()
+    # v.add_image(stain)
+    # v.add_image(stain_averaged, scale=(16, 16, 16))
+    # v.add_labels(mask)
+    # # v.add_labels(mask, scale=(16, 16, 16))
+    # v.add_labels(seg_extended)
+    # napari.run()
+
+    return mask
 
 
 def gfp_annotation(
@@ -50,7 +145,7 @@ def gfp_annotation(
     # Extend the sgns so that they cover the SGN boundaries.
     # sgns_extended = _extend_seg(gfp, sgns)
     # TODO we need to integrate this directly in the object measurement to efficiently do it at scale.
-    seg_extended = annotation_utils._extend_seg_simple(seg, dilation=4)
+    seg_extended = _extend_seg_simple(seg, dilation=4)
     if is_otof:
         seg_extended = seg.copy()
 
@@ -59,7 +154,7 @@ def gfp_annotation(
         mask = None
         feature_set = "default"
     else:
-        mask = annotation_utils._create_mask(seg_extended, stain1)
+        mask = _create_mask(seg_extended, stain1)
         assert mask.shape == seg_extended.shape
         feature_set = "default_background_norm" if background_norm == "division" else "default_background_subtract"
 
@@ -101,7 +196,7 @@ def gfp_annotation(
     # Add widgets:
 
     # 1.) The widget for selcting the statistics to be used and displaying the histogram.
-    stat_widget = annotation_utils._create_stat_widget(statistics, default_stat)
+    stat_widget = _create_stat_widget(statistics, default_stat)
 
     # 2.) Precompute statistic ranges.
     stat_names = stat_widget.stat_names
@@ -160,26 +255,3 @@ def gfp_annotation(
     stat_widget.setWindowTitle(f"{stain1_name} Histogram")
 
     napari.run()
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Start a GUI for determining an intensity threshold for positive "
-        "/ negative transduction in segmented cells.")
-    parser.add_argument("prefix", help="The prefix of the files to open with the annotation tool.")
-    parser.add_argument("-b", "--background_norm",
-                        help="How to normalize the intensity values for background intensity."
-                        "Valid options are 'division' and 'subtraction'."
-                        "If nothing is passed then the intensity values are not normalized.")
-    parser.add_argument("--otof", action="store_true",
-                        help="Whether to run the annotation tool for otof samples with VGlut3, "
-                        "Alphatag and IHC segmentation.")  # noqa
-    parser.add_argument("--seg_version", type=str, default=None,
-                        help="Supply segmentation version, e.g. SGN_v2, to use intensities from object measure table.")
-    args = parser.parse_args()
-
-    gfp_annotation(args.prefix, background_norm=args.background_norm, is_otof=args.otof, seg_version=args.seg_version)
-
-
-if __name__ == "__main__":
-    main()
--- scripts/intensity_annotation/rbOtof_annotation.py
+++ scripts/intensity_annotation/rbOtof_annotation.py
@@ -5,133 +5,11 @@ import imageio.v3 as imageio
 import napari
 import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QComboBox
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from magicgui import magicgui
 
-from elf.parallel.distance_transform import distance_transform
-from elf.parallel.seeded_watershed import seeded_watershed
-
+import flamingo_tools.intensity_annotation.annotation_utils as annotation_utils
 from flamingo_tools.measurements import get_object_measures_from_table
 from flamingo_tools.s3_utils import get_s3_path
-
-
-class HistogramWidget(QWidget):
-    """Qt widget that draws/updates a histogram for one napari layer."""
-    def __init__(self, statistics, default_stat, bins: int = 32, parent=None):
-        super().__init__(parent)
-        self.bins = bins
-
-        # --- layout ------------------------------------------------------
-        self.fig, self.ax = plt.subplots(figsize=(4, 3), tight_layout=True)
-        self.canvas = FigureCanvasQTAgg(self.fig)
-
-        # We exclude the label id and the volume / surface measurements.
-        self.stat_names = statistics.columns[1:-2] if len(statistics.columns) > 2 else statistics.columns[1:]
-        self.param_choices = self.stat_names
-
-        self.param_box = QComboBox()
-        self.param_box.addItems(self.param_choices)
-        self.param_box.setCurrentText(default_stat)
-
-        self.refresh_btn = QPushButton("Refresh")
-        self.refresh_btn.clicked.connect(self.update_hist)
-
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Choose statistic:"))
-        layout.addWidget(self.param_box)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.refresh_btn)
-        self.setLayout(layout)
-
-        self.statistics = statistics
-        self.update_hist()  # initial draw
-
-    def update_hist(self):
-        """Redraw the histogram."""
-        self.ax.clear()
-
-        stat_name = self.param_box.currentText()
-
-        data = self.statistics[stat_name]
-        # Seaborn version (nicer aesthetics)
-        sns.histplot(data, bins=self.bins, ax=self.ax, kde=False)
-
-        self.ax.set_xlabel(f"{stat_name} Marker Intensity")
-        self.ax.set_ylabel("Count")
-        self.canvas.draw_idle()
-
-
-def _create_stat_widget(statistics, default_stat):
-    widget = HistogramWidget(statistics, default_stat)
-    return widget
-
-
-# Extend via watershed, this could work for a better alignment.
-def _extend_seg_complex(stain, seg):
-    # 1.) compute distance to the SGNs to create the background seed.
-    print("Compute edt")
-
-    # Could use parallel impl
-    distance_threshol = 7
-    distances = distance_transform(seg == 0)
-
-    # Erode seeds?
-    seeds = seg.copy()
-    bg_seed_id = int(seeds.max()) + 1
-    seeds[distances > distance_threshol] = bg_seed_id
-
-    # Dilate to cover everything on the boundary?
-    print("Run watershed")
-    seg_extended = seeded_watershed(stain, markers=seeds)
-    seg_extended[seg_extended == bg_seed_id] = 0
-
-    v = napari.Viewer()
-    v.add_image(stain)
-    v.add_image(distances)
-    v.add_labels(seeds)
-    v.add_labels(seg_extended)
-    napari.run()
-
-
-# Just dilate by 3 pixels.
-def _extend_seg_simple(stain, seg, dilation):
-    block_shape = (128,) * 3
-    halo = (dilation + 2,) * 3
-
-    distances = distance_transform(seg == 0, block_shape=block_shape, halo=halo, n_threads=8)
-    mask = distances < dilation
-
-    seg_extended = np.zeros_like(seg)
-    seg_extended = seeded_watershed(
-        distances, seg, seg_extended, block_shape=block_shape, halo=halo, n_threads=8, mask=mask
-    )
-
-    return seg_extended
-
-
-def _create_mask(seg_extended, stain):
-    from skimage.transform import downscale_local_mean, resize
-
-    stain_averaged = downscale_local_mean(stain, factors=(16, 16, 16))
-    # The 35th percentile seems to be a decent approximation for the background subtraction.
-    threshold = np.percentile(stain_averaged, 35)
-    mask = stain_averaged > threshold
-    mask = resize(mask, seg_extended.shape, order=0, anti_aliasing=False, preserve_range=True).astype(bool)
-    mask[seg_extended != 0] = 0
-
-    # v = napari.Viewer()
-    # v.add_image(stain)
-    # v.add_image(stain_averaged, scale=(16, 16, 16))
-    # v.add_labels(mask)
-    # # v.add_labels(mask, scale=(16, 16, 16))
-    # v.add_labels(seg_extended)
-    # napari.run()
-
-    return mask
 
 
 def otof_annotation(prefix, seg_version="IHC_LOWRES-v3", default_stat="median"):
@@ -159,7 +37,7 @@ def otof_annotation(prefix, seg_version="IHC_LOWRES-v3", default_stat="median"):
     # Extend the sgns so that they cover the SGN boundaries.
     # sgns_extended = _extend_seg(gfp, sgns)
     # TODO we need to integrate this directly in the object measurement to efficiently do it at scale.
-    seg_extended = _extend_seg_simple(stain1, seg, dilation=4)
+    seg_extended = annotation_utils._extend_seg_simple(seg, dilation=4)
     # Compute the intensity statistics.
     mask = None
 
@@ -195,7 +73,7 @@ def otof_annotation(prefix, seg_version="IHC_LOWRES-v3", default_stat="median"):
     # Add widgets:
 
     # 1.) The widget for selcting the statistics to be used and displaying the histogram.
-    stat_widget = _create_stat_widget(statistics, default_stat)
+    stat_widget = annotation_utils._create_stat_widget(statistics, default_stat)
 
     # 2.) Precompute statistic ranges.
     stat_names = stat_widget.stat_names
