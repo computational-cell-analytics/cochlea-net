ff84afcb5f7214202057edff3ad93c01f66f1f74
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-08-20 20:37:00 +0200

    Add roi

--- export.sh
+++ export.sh
@@ -2,4 +2,4 @@ python scripts/export_data/export_by_position.py \
   -c M_LR_000227_R -s 2 -o mlr227r-for-lennart \
   --axis 0 \
   --groups ihc --channels PV VGlut3 CTBP2 \
-  --view
+  --roi_halo 512 512 24 \
