5efcbc17c311fef23c458d29498d41bac8284fcc
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-08-21 21:13:22 +0200

    Fix export script

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 python scripts/export_data/export_by_position.py \
   -c M_LR_000227_R -s 2 -o mlr227r-for-lennart \
-  --axis 0 \
   --groups ihc --channels PV VGlut3 CTBP2 \
-  --roi_halo 512 512 24 \
+  --roi_halo 300 300 16 \
+  --scale 1
